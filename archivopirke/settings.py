0cc2ca47f0ca77e0542ff89bfc92ba638476a2bb
chivke <chivke@pm.me>
2019-11-27 13:50:58 -0300

    add allowed host to centrodememoria.pirque

--- archivopirke/settings.py
+++ archivopirke/settings.py
@@ -67,7 +67,7 @@ else:
             }
         }
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'centrodememoria.pirque']
 #CSRF_COOKIE_SECURE = True
 #SESSION_COOKIE_SECURE = True
 

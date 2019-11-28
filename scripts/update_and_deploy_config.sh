4b3cd082e7661c3a29fb5f4fb71a089120c76ce4
chivke <chivke@pm.me>
2019-11-28 15:07:25 -0300

    comments options in script bash.

--- scripts/update_and_deploy_config.sh
+++ scripts/update_and_deploy_config.sh
@@ -6,11 +6,11 @@ git pull
 sed -i 's/SECRET_DEPLOY = False/SECRET_DEPLOY = True/g' /srv/cmpirque/archivopirke/settings.py
 sed -i 's/DEBUG = True/DEBUG = False/g' /srv/cmpirque/archivopirke/settings.py
 sed -i 's/DB_DEBUG = True/DB_DEBUG = False/g' /srv/cmpirque/archivopirke/settings.py
-sed -i 's/#CSRF_COOKIE_SECURE/CSRF_COOKIE_SECURE/g' /srv/cmpirque/archivopirke/settings.py
-sed -i 's/#SESSION_COOKIE_SECURE/SESSION_COOKIE_SECURE/g' /srv/cmpirque/archivopirke/settings.py
-sed -i 's/#X_FRAME_OPTIONS/X_FRAME_OPTIONS/g' /srv/cmpirque/archivopirke/settings.py
-sed -i 's/#SECURE_CONTENT_TYPE_NOSNIFF/SECURE_CONTENT_TYPE_NOSNIFF/g' /srv/cmpirque/archivopirke/settings.py
-sed -i 's/#SECURE_BROWSER_XSS_FILTER/SECURE_BROWSER_XSS_FILTER/g' /srv/cmpirque/archivopirke/settings.py
+#sed -i 's/#CSRF_COOKIE_SECURE/CSRF_COOKIE_SECURE/g' /srv/cmpirque/archivopirke/settings.py
+#sed -i 's/#SESSION_COOKIE_SECURE/SESSION_COOKIE_SECURE/g' /srv/cmpirque/archivopirke/settings.py
+#sed -i 's/#X_FRAME_OPTIONS/X_FRAME_OPTIONS/g' /srv/cmpirque/archivopirke/settings.py
+#sed -i 's/#SECURE_CONTENT_TYPE_NOSNIFF/SECURE_CONTENT_TYPE_NOSNIFF/g' /srv/cmpirque/archivopirke/settings.py
+#sed -i 's/#SECURE_BROWSER_XSS_FILTER/SECURE_BROWSER_XSS_FILTER/g' /srv/cmpirque/archivopirke/settings.py
 echo 'Deploy configuration:'
 cat /srv/cmpirque/archivopirke/settings.py | grep "^SECRET_DEPLOY"
 cat /srv/cmpirque/archivopirke/settings.py | grep "^DEBUG"

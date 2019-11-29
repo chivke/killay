27d1e84c1ae8fd0184e2eda8f31f96f67dc892fd
chivke <chivke@pm.me>
2019-11-29 10:22:51 -0300

    modifica campo extendido de página de djangocms y agrega script de implementación.

--- archivopirke/models.py
+++ archivopirke/models.py
@@ -1,17 +1,13 @@
 from cms.extensions import PageExtension
 from cms.extensions.extension_pool import extension_pool
 from filer.fields.file import FilerFileField
+from django.db import models
+
 
 class HeaderExtension(PageExtension):
-    header_image = FilerFileField(
+    header_image = models.FileField(
         help_text="Page header image/ imagen de cabecera para página ",
-        related_name="header_image_headerextension",
-        blank=True,
-        null=True,
-        )
-    subheader_image = FilerFileField(
-        help_text="Page subheader image/ imagen de subcabecera para página ",
-        related_name="subheader_image_headerextension",
+        #related_name="header_image_headerextension",
         blank=True,
         null=True,
         )

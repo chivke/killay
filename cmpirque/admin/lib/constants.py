75625695f8d239fd033d29d6a9c40bdfebac7034
chivke <sergio.acuna@cornershopapp.com>
2021-08-27 14:16:59 -0400

    feat(admin): improves models and change name of social media for provider.

---
+++ cmpirque/admin/lib/constants.py
@@ -0,0 +1,13 @@
+class AdminConfigurationConstants:
+    FACEBOOK = "facebook"
+    TWITTER = "twitter"
+    INSTAGRAM = "instagram"
+    YOUTUBE = "youtube"
+    VIMEO = "vimeo"
+    PROVIDER_CHOICES = [
+        (FACEBOOK, "Facebook"),
+        (TWITTER, "Twitter"),
+        (INSTAGRAM, "Instagram"),
+        (YOUTUBE, "Youtube"),
+        (VIMEO, "Vimeo"),
+    ]
--- cmpirque/admin/models.py
+++ cmpirque/admin/models.py
@@ -1,18 +1,32 @@
 from django.conf import settings
 from django.db import models
 
+from cmpirque.admin.lib.constants import AdminConfigurationConstants
+
+
+class AdminConfigurationManager(models.Manager):
+    def current(self):
+        return self.filter(active=True).first()
+
 
 class AdminConfiguration(models.Model):
     site_name = models.CharField(max_length=150, default=settings.SITE_NAME)
     active = models.BooleanField(default=True)
     is_published = models.BooleanField(default=True)
 
+    objects = AdminConfigurationManager()
+
 
 class SocialMedia(models.Model):
     config = models.ForeignKey(
         AdminConfiguration, on_delete=models.CASCADE, related_name="social_medias"
     )
-    name = models.CharField(max_length=150)
+    provider = models.CharField(
+        choices=AdminConfigurationConstants.PROVIDER_CHOICES,
+        max_length=150,
+        null=False,
+        blank=False,
+    )
     url = models.URLField()
     css_class = models.CharField(max_length=150, null=True, blank=True)
     is_visible = models.BooleanField(default=False)

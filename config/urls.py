2fb9e2f3d586f878d7dbe1bbfc922f946f9336a7
chivke <sergio.acuna@cornershopapp.com>
2021-08-27 00:39:02 -0400

    fix(urls): remove legacy admin urls.

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.contrib import admin
+
+# from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 from django.conf.urls.static import static
@@ -12,7 +13,7 @@ urlpatterns = [
     path("pages/", include("cmpirque.pages.urls", namespace="pages")),
     path("users/", include("cmpirque.users.urls", namespace="users")),
     path("videos/", include("cmpirque.videos.urls", namespace="videos")),
-    path("admin/", admin.site.urls),
+    #    path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

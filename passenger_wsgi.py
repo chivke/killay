fcf898170dcd23825c17dd3a4dcf8b77288eb981
chivke <sergio.acuna@cornershopapp.com>
2022-01-10 17:53:20 -0300

    fix(passenger): fix the entrypoint to handler correctly the root dirname

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,16 +3,17 @@ import sys
 
 
 def get_application():
-	root_dir = os.path.dirname(__file__)
-	virtual_env = os.path.join(
-		root_dir, "env", 'bin', 'python'
-	)
-	if sys.executable != virtual_env:
-		os.execl(virtual_env, virtual_env, *sys.argv)
-	sys.path.insert(0, os.path.join(os.getcwd(), virtual_env, 'bin'))
-	sys.path.append(os.path.join(root_dir, "killay"))
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-	from django.core.wsgi import get_wsgi_application
-	return get_wsgi_application()
+    root_dir = os.path.dirname(__file__)
+    virtual_env = os.path.join(
+        root_dir, "env", 'bin', 'python'
+    )
+    if sys.executable != virtual_env:
+        os.execl(virtual_env, virtual_env, *sys.argv)
+    sys.path.insert(0, os.path.join(os.getcwd(), virtual_env, 'bin'))
+    sys.path.append(os.path.join(root_dir, "killay"))
+    sys.path.append(os.path.join(root_dir))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+    from django.core.wsgi import get_wsgi_application
+    return get_wsgi_application()
 
 application = get_application()

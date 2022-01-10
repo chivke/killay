5ffedb498d150e07357136d44f1fec4526f4ec71
chivke <sergio.acuna@cornershopapp.com>
2022-01-10 14:12:51 -0300

    feat(prod): fix config setting file for production and add passenger wsgi file.

---
+++ passenger_wsgi.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+
+def get_application():
+	virtual_env_directory = "env"
+	virtual_env = os.path.join(
+		os.getcwd(), virtual_env_directory, 'bin', 'python'
+	)
+	if sys.executable != virtual_env:
+		os.execl(virtual_env, virtual_env, *sys.argv)
+	sys.path.insert(0, os.path.join(os.getcwd(), virtual_env, 'bin'))
+
+	from config import wsgi
+
+	return wsgi.application
+
+application = get_application()

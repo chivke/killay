dcbb1b7b0b143d2b30278cd4cf2f29b3a2851112
chivke <sergio.acuna@cornershopapp.com>
2022-01-10 17:18:41 -0300

    fix(passenger): fix the entrypoint to handler correctly the root dirname

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,8 +5,9 @@ import sys
 def get_application():
 	virtual_env_directory = "env"
 	virtual_env = os.path.join(
-		os.getcwd(), virtual_env_directory, 'bin', 'python'
+		os.path.dirname(__file__), virtual_env_directory, 'bin', 'python'
 	)
+	print(virtual_env)
 	if sys.executable != virtual_env:
 		os.execl(virtual_env, virtual_env, *sys.argv)
 	sys.path.insert(0, os.path.join(os.getcwd(), virtual_env, 'bin'))

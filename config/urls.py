d6467259527a132760e13764dd5bb40021252219
Sergio Acuña <sergioacunao@protonmail.com>
2023-05-19 15:48:22 -0400

    chore(40X): update urls and add 400 template

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
-from django.conf.urls.static import static
+from django.views import defaults as default_views
 
 from killay.pages.views import home_page_view
 
@@ -25,3 +25,22 @@ if settings.DEBUG:
         import debug_toolbar
 
         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]

39a8dbbf79ef50318fa0c42d7e5e89304a9f1574
Sergio Acuña <sergioacunao@protonmail.com>
2023-05-24 11:47:25 -0400

    chore(admin.forms): add PersonForm, KeywordForm, PieceForm, PieceMetaForm

--- killay/admin/forms.py
+++ killay/admin/forms.py
@@ -2,7 +2,16 @@ from django import forms
 
 from killay.admin.models import Logo, SiteConfiguration, SocialMedia
 from killay.admin.utils import ImageFileInput
-from killay.archives.models import Archive, Category, Collection
+from killay.archives.lib.constants import PieceConstants
+from killay.archives.models import (
+    Archive,
+    Category,
+    Collection,
+    Keyword,
+    Person,
+    Piece,
+    PieceMeta,
+)
 
 
 class SiteConfigurationForm(forms.ModelForm):
@@ -46,9 +55,7 @@ class ArchiveForm(forms.ModelForm):
         model = Archive
         fields = ["name", "slug", "description", "position"]
 
-    description = forms.CharField(
-        required=False, widget=forms.Textarea(attrs={"rows": 1})
-    )
+    description = forms.CharField(required=False, widget=forms.Textarea())
 
 
 class ArchiveListForm(forms.ModelForm):
@@ -69,10 +76,12 @@ ArchiveFormSet = forms.modelformset_factory(
 class CollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
-        fields = ["name", "slug", "description", "position", "archive"]
+        fields = ["archive", "name", "slug", "description", "position"]
 
-    description = forms.CharField(
-        required=False, widget=forms.Textarea(attrs={"rows": 1})
+    archive = forms.ModelChoiceField(
+        queryset=Archive.objects_in_site.all(),
+        widget=forms.Select(attrs={"class": "ui fluid dropdown"}),
+        required=False,
     )
 
 
@@ -83,6 +92,11 @@ class CollectionListForm(forms.ModelForm):
 
     name = forms.CharField(disabled=True)
     slug = forms.CharField(disabled=True)
+    archive = forms.ModelChoiceField(
+        queryset=Archive.objects_in_site.all(),
+        widget=forms.Select(attrs={"class": "ui fluid dropdown"}),
+        required=False,
+    )
 
 
 CollectionFormSet = forms.modelformset_factory(
@@ -95,11 +109,150 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ["name", "slug", "description", "position"]
 
-    description = forms.CharField(
-        required=False, widget=forms.Textarea(attrs={"rows": 1})
-    )
+    description = forms.CharField(required=False, widget=forms.Textarea())
+
+
+class CategoryListForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["name", "slug", "position"]
+
+    name = forms.CharField(disabled=True)
+    slug = forms.CharField(disabled=True)
 
 
 CategoryFormSet = forms.modelformset_factory(
-    Category, form=CategoryForm, extra=1, can_delete=True
+    Category, form=CategoryListForm, extra=0, can_delete=False
 )
+
+
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = ["name", "slug", "description", "position"]
+
+    description = forms.CharField(required=False, widget=forms.Textarea())
+
+
+class PersonListForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = ["name", "slug", "position"]
+
+    name = forms.CharField(disabled=True)
+    slug = forms.CharField(disabled=True)
+
+
+PersonFormSet = forms.modelformset_factory(
+    Person, form=PersonListForm, extra=0, can_delete=False
+)
+
+
+class KeywordForm(forms.ModelForm):
+    class Meta:
+        model = Keyword
+        fields = ["name", "slug", "description", "position"]
+
+    description = forms.CharField(required=False, widget=forms.Textarea())
+
+
+class KeywordListForm(forms.ModelForm):
+    class Meta:
+        model = Keyword
+        fields = ["name", "slug", "position"]
+
+    name = forms.CharField(disabled=True)
+    slug = forms.CharField(disabled=True)
+
+
+KeywordFormSet = forms.modelformset_factory(
+    Keyword, form=KeywordListForm, extra=0, can_delete=False
+)
+
+
+CollectionFormField = forms.ModelChoiceField(
+    queryset=Collection.objects_in_site.all(),
+    widget=forms.Select(attrs={"class": "ui fluid dropdown"}),
+    required=True,
+)
+
+
+class PieceForm(forms.ModelForm):
+    class Meta:
+        model = Piece
+        fields = [
+            "collection",
+            "is_published",
+            "code",
+            "title",
+            "kind",
+            "thumb",
+            "categories",
+            "people",
+            "keywords",
+        ]
+
+    thumb = forms.ImageField(widget=ImageFileInput())
+    kind = forms.ChoiceField(
+        choices=PieceConstants.KIND_CHOICES,
+        widget=forms.Select(attrs={"class": "ui fluid dropdown"}),
+        required=True,
+    )
+    collection = CollectionFormField
+    categories = forms.ModelMultipleChoiceField(
+        queryset=Category.objects_in_site.all(),
+        widget=forms.SelectMultiple(attrs={"class": "ui fluid dropdown"}),
+        required=False,
+    )
+    people = forms.ModelMultipleChoiceField(
+        queryset=Person.objects_in_site.all(),
+        widget=forms.SelectMultiple(attrs={"class": "ui fluid dropdown"}),
+        required=False,
+    )
+    keywords = forms.ModelMultipleChoiceField(
+        queryset=Keyword.objects_in_site.all(),
+        widget=forms.SelectMultiple(attrs={"class": "ui fluid dropdown"}),
+        required=False,
+    )
+
+
+class PieceListForm(forms.ModelForm):
+    class Meta:
+        model = Piece
+        fields = [
+            "code",
+            "kind",
+            "title",
+            "thumb",
+            "is_published",
+        ]
+
+    code = forms.CharField(disabled=True)
+    kind = forms.CharField(disabled=True)
+    title = forms.CharField(disabled=True)
+    thumb = forms.ImageField(widget=ImageFileInput(), disabled=True)
+
+
+PieceFormSet = forms.modelformset_factory(
+    Piece, form=PieceListForm, extra=0, can_delete=False
+)
+
+
+class PieceMetaForm(forms.ModelForm):
+    class Meta:
+        model = PieceMeta
+        fields = [
+            "event",
+            "description",
+            "description_date",
+            "location",
+            "duration",
+            "register_date",
+            "register_author",
+            "productor",
+            "notes",
+            "archivist_notes",
+            "documentary_unit",
+            "lang",
+            "original_format",
+        ]

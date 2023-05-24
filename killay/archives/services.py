42defad6e58297210b26d80c911bf4bee6b06166
Sergio Acuña <sergioacunao@protonmail.com>
2023-05-24 12:35:29 -0400

    chore(archives.services): add get_collection_related_field_data,get_archive_names_and_slugs, get_collection_names_and_slugs

--- killay/archives/services.py
+++ killay/archives/services.py
@@ -7,6 +7,21 @@ def get_archive_related_field_data(archive_id: int) -> dict:
     total_collections = Collection.objects.filter(archive_id=archive_id).count()
     total_pieces = Piece.objects.filter(collection__archive_id=archive_id).count()
     return {
-        gettext("Collections"): total_collections,
-        gettext("Pieces"): total_pieces,
+        Collection._meta.verbose_name_plural.capitalize(): total_collections,
+        Piece._meta.verbose_name_plural.capitalize(): total_pieces,
     }
+
+
+def get_collection_related_field_data(collection_id: int) -> dict:
+    total_pieces = Piece.objects.filter(collection_id=collection_id).count()
+    return {
+        Piece._meta.verbose_name_plural.capitalize(): total_pieces,
+    }
+
+
+def get_archive_names_and_slugs() -> list:
+    return list(Archive.objects_in_site.values("name", "slug"))
+
+
+def get_collection_names_and_slugs() -> list:
+    return list(Collection.objects_in_site.values("name", "slug"))

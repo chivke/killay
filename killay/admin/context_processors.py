222f400497ff82b7c0ddc87b14fc9ec0102e185a
Sergio Acuña <sergioacunao@protonmail.com>
2023-05-29 20:25:14 -0400

    chore(admin.context_processors): improve site_context

--- killay/admin/context_processors.py
+++ killay/admin/context_processors.py
@@ -1,6 +1,3 @@
-from killay.admin.models import SiteConfiguration
-
-
 def site_context(request):
-    conf = SiteConfiguration.objects.current()
-    return {"site_conf": conf}
+    conf = request.site_configuration
+    return {"site_conf": conf, "ip_address": request.ip_address}

a5eb1919acecbd32f4f7342de235df59fb7bcf25
Sergio Acuña <sergioacunao@protonmail.com>
2023-05-29 22:19:54 -0400

    fix(*tests): use client before RequestFactory

--- killay/admin/tests/test_views/test_configuration.py
+++ killay/admin/tests/test_views/test_configuration.py
@@ -1,10 +1,10 @@
 import pytest
 
-from django.test import RequestFactory
+from django.test import RequestFactory, Client
 from django.urls import resolve
 
 from killay.admin.models import SiteConfiguration
-from killay.admin.views.configuration import admin_configuration_view
+from killay.admin.views.configuration import admin_site_configuration_view
 
 from killay.users.models import User
 
@@ -26,35 +26,25 @@ LOGO_FORMSET_DATA = {"logos-INITIAL_FORMS": "0", "logos-TOTAL_FORMS": "0"}
 SITE_CONF_DATA = {**SOCIALMEDIA_FORMSET_DATA, **LOGO_FORMSET_DATA}
 
 
-class TestSiteConfigurationView:
-    def test_get(self, admin_user: User, rf: RequestFactory):
-        request = rf.get("/admin/")
-        request.resolver_match = resolve("/admin/")
-        request.user = admin_user
-        response = admin_configuration_view(request)
+class TestConfigurationUpdateView:
+    def test_get(self, admin_user: User, client: Client):
+        client.force_login(admin_user)
+        response = client.get("/admin/conf/")
         assert response.render()
         assert response.status_code == 200
 
-    def test_update(self, admin_user: User, rf: RequestFactory):
+    def test_update(self, admin_user: User, client: Client):
         data = {"name": "Other site name", "domain": "ex.org", "is_published": False}
-        request = rf.post("/admin/", {**data, **SITE_CONF_DATA})
-        request.user = admin_user
-        response = admin_configuration_view(request)
+        client.force_login(admin_user)
+        response = client.post("/admin/conf/", {**data})
         assert response.status_code == 302
         conf = SiteConfiguration.objects.current()
         for field, value in data.items():
             assert getattr(conf, field) == value
-        social_media = conf.social_medias.first()
-        assert social_media.provider == "youtube"
-        assert social_media.url == "http://youtube.com"
-        assert social_media.is_visible is True
 
-    def test_update_fail(self, admin_user: User, rf: RequestFactory):
-        data = {**SOCIALMEDIA_FORMSET_DATA}
-        data.pop("social_medias-0-provider")
-        request = rf.post("/admin/", SITE_CONF_DATA)
-        request.user = admin_user
-        response = admin_configuration_view(request)
+    def test_update_fail(self, admin_user: User, client: Client):
+        data = {"name": ""}
+        client.force_login(admin_user)
+        response = client.post("/admin/conf/", data)
         assert response.status_code == 200
         assert response.context_data["form"].errors
-        assert response.context_data["formset"].errors

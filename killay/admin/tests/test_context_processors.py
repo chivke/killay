d37b36fa697f7dc33992a011502c9eccc3d175df
Sergio Acuña <sergioacunao@protonmail.com>
2023-05-29 22:15:40 -0400

    fix(admin.tests.test_context_processors): fix test_site_context

--- killay/admin/tests/test_context_processors.py
+++ killay/admin/tests/test_context_processors.py
@@ -1,15 +1,20 @@
 import pytest
+from unittest import mock
 
 from django.conf import settings
 from django.test import RequestFactory
 
 from killay.admin.context_processors import site_context
+from killay.admin.middleware import SiteConfigurationMiddleware
 
 pytestmark = pytest.mark.django_db
 
 
 def test_site_context(rf: RequestFactory):
+    get_response = mock.Mock()
     request = rf.get("/")
+    middleware = SiteConfigurationMiddleware(get_response)
+    middleware(request)
     site = site_context(request)
     assert "site_conf" in site
     assert site["site_conf"].name == settings.SITE_NAME

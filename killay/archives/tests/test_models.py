48c6ba17478e50712f9a0ade74294a7bf3ec8b8b
Sergio Acuña <sergioacunao@protonmail.com>
2023-07-21 17:05:57 -0400

    chore(archives.models): apply several migratrions

--- killay/archives/tests/test_models.py
+++ killay/archives/tests/test_models.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 import pytest
 
 from killay.archives.tests import recipes as archives_recipes
@@ -16,12 +18,26 @@ class TestCollection:
         collection = archives_recipes.collection_recipe.make()
         assert str(collection) == f"{collection.name} <{collection.slug}>"
 
+    def test_archive_slug(self):
+        collection = archives_recipes.collection_recipe.make()
+        assert collection.archive_slug == collection.archive.slug
+
 
 @pytest.mark.django_db
 class TestCategory:
     def test_str(self):
         category = archives_recipes.category_recipe.make()
         assert str(category) == category.name
+
+    def test_archive_slug(self):
+        collection = archives_recipes.collection_recipe.make()
+        category = archives_recipes.category_recipe.make(collection_id=collection.id)
+        assert category.archive_slug == collection.archive.slug
+
+    def test_collection_slug(self):
+        collection = archives_recipes.collection_recipe.make()
+        category = archives_recipes.category_recipe.make(collection_id=collection.id)
+        assert category.collection_slug == collection.slug
 
 
 @pytest.mark.django_db
@@ -44,12 +60,25 @@ class TestPiece:
         piece = archives_recipes.piece_recipe.make()
         assert str(piece) == f"Piece <{piece.code}>"
 
+    def test_active_provider(self):
+        piece = archives_recipes.piece_recipe.make()
+        provider = archives_recipes.provider_recipe.make(active=True, piece_id=piece.id)
+        assert piece.active_provider.id == provider.id
+
 
 @pytest.mark.django_db
 class TestSequence:
     def test_str(self):
         sequence = archives_recipes.sequence_recipe.make()
         assert str(sequence) == f"Sequence <{sequence.id}>"
+
+    def test_ini_sec(self):
+        sequence = archives_recipes.sequence_recipe.make(ini=time(1))
+        assert sequence.ini_sec == 3600
+
+    def test_end_sec(self):
+        sequence = archives_recipes.sequence_recipe.make(end=time(1))
+        assert sequence.ini_sec == 3600
 
 
 @pytest.mark.django_db

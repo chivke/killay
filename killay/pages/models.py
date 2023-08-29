599e889d983853758ae9ec58cd2e8ab0330cc5bc
Sergio Acuña <sergioacunao@protonmail.com>
2023-08-28 20:53:00 -0400

    chore(pages.models): add help text to Page model fields

--- killay/pages/models.py
+++ killay/pages/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from django.db import models
 from django.urls import reverse
-from django.utils.translation import gettext_lazy
 from django_quill.fields import QuillField
 
 from killay.pages.lib.constants import PageConstants
@@ -11,28 +10,66 @@ from killay.admin.utils import InSiteManager
 
 class Page(models.Model):
     title = models.CharField(
-        PageConstants.NAME_TITLE, max_length=150, null=False, blank=False
+        verbose_name=PageConstants.FIELD_TITLE,
+        help_text=PageConstants.FIELD_TITLE_HELP_TEXT,
+        max_length=150,
+        null=False,
+        blank=False,
     )
     slug = models.SlugField(
-        PageConstants.NAME_SLUG, max_length=150, null=False, blank=False
+        verbose_name=PageConstants.FIELD_SLUG,
+        help_text=PageConstants.FIELD_SLUG_HELP_TEXT,
+        max_length=150,
+        null=False,
+        blank=False,
     )
     kind = models.CharField(
-        verbose_name=PageConstants.NAME_KIND,
+        verbose_name=PageConstants.FIELD_KIND,
+        help_text=PageConstants.FIELD_KIND_HELP_TEXT,
         choices=PageConstants.KIND_CHOICES,
         max_length=10,
         default=PageConstants.KIND_PAGE,
     )
-    created_at = models.DateTimeField(PageConstants.NAME_CREATED_AT, auto_now_add=True)
-    updated_at = models.DateTimeField(PageConstants.NAME_UPDATED_AT, auto_now=True)
-    is_visible = models.BooleanField(PageConstants.NAME_IS_VISIBLE, default=False)
-    body = QuillField(PageConstants.NAME_BODY, null=True, blank=True)
-    header_image = models.ImageField(
-        PageConstants.NAME_HEADER_IMAGE, upload_to="page_images", null=True, blank=True
+    created_at = models.DateTimeField(
+        verbose_name=PageConstants.FIELD_CREATED_AT,
+        auto_now_add=True,
     )
-    redirect_to = models.URLField(PageConstants.NAME_REDIRECT_TO, null=True, blank=True)
-    position = models.PositiveSmallIntegerField(PageConstants.NAME_POSITION, default=0)
+    updated_at = models.DateTimeField(
+        verbose_name=PageConstants.FIELD_UPDATED_AT, auto_now=True
+    )
+    is_visible = models.BooleanField(
+        verbose_name=PageConstants.FIELD_IS_VISIBLE,
+        help_text=PageConstants.FIELD_IS_VISIBLE_HELP_TEXT,
+        default=False,
+    )
+    body = QuillField(
+        verbose_name=PageConstants.FIELD_BODY,
+        help_text=PageConstants.FIELD_BODY_HELP_TEXT,
+        null=True,
+        blank=True,
+    )
+    header_image = models.ImageField(
+        verbose_name=PageConstants.FIELD_HEADER_IMAGE,
+        help_text=PageConstants.FIELD_HEADER_IMAGE_HELP_TEXT,
+        upload_to="page_images",
+        null=True,
+        blank=True,
+    )
+    redirect_to = models.URLField(
+        verbose_name=PageConstants.FIELD_REDIRECT_TO,
+        help_text=PageConstants.FIELD_REDIRECT_TO_HELP_TEXT,
+        null=True,
+        blank=True,
+    )
+    position = models.PositiveSmallIntegerField(
+        verbose_name=PageConstants.FIELD_POSITION,
+        help_text=PageConstants.FIELD_POSITION_HELP_TEXT,
+        default=0,
+    )
     archive = models.ForeignKey(
         "archives.Archive",
+        verbose_name=PageConstants.FIELD_ARCHIVE,
+        help_text=PageConstants.FIELD_ARCHIVE_HELP_TEXT,
         on_delete=models.SET_NULL,
         related_name="pages",
         null=True,
@@ -40,6 +77,8 @@ class Page(models.Model):
     )
     collection = models.ForeignKey(
         "archives.Collection",
+        verbose_name=PageConstants.FIELD_COLLECTION,
+        help_text=PageConstants.FIELD_COLLECTION_HELP_TEXT,
         on_delete=models.SET_NULL,
         related_name="pages",
         null=True,
@@ -47,6 +86,8 @@ class Page(models.Model):
     )
     place = models.ForeignKey(
         "archives.Place",
+        verbose_name=PageConstants.FIELD_PLACE,
+        help_text=PageConstants.FIELD_PLACE_HELP_TEXT,
         on_delete=models.SET_NULL,
         related_name="pages",
         null=True,
@@ -57,13 +98,19 @@ class Page(models.Model):
     )
 
     is_visible_in_navbar = models.BooleanField(
-        gettext_lazy("Is visible in navbar"), default=False
+        verbose_name=PageConstants.FIELD_IS_VISIBLE_IN_NAVBAR,
+        help_text=PageConstants.FIELD_IS_VISIBLE_IN_NAVBAR_HELP_TEXT,
+        default=False,
     )
     is_visible_in_footer = models.BooleanField(
-        gettext_lazy("Is visible in footer"), default=False
+        verbose_name=PageConstants.FIELD_IS_VISIBLE_IN_FOOTER,
+        help_text=PageConstants.FIELD_IS_VISIBLE_IN_FOOTER_HELP_TEXT,
+        default=False,
     )
     is_title_visible_in_body = models.BooleanField(
-        gettext_lazy("Is title visible in body"), default=False
+        verbose_name=PageConstants.FIELD_IS_TITLE_VISIBLE_IN_BODY,
+        help_text=PageConstants.FIELD_IS_TITLE_VISIBLE_IN_BODY_HELP_TEXT,
+        default=False,
     )
     # to deprecate
     collection_site = models.ForeignKey(
@@ -76,8 +123,8 @@ class Page(models.Model):
 
     class Meta:
         unique_together = ["slug", "site"]
-        verbose_name = gettext_lazy("page")
-        verbose_name_plural = gettext_lazy("pages")
+        verbose_name = PageConstants.VERBOSE_NAME
+        verbose_name_plural = PageConstants.VERBOSE_NAME_PLURAL
         ordering = ["position"]
 
     objects = models.Manager()

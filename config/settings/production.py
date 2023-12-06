767f6e7943a0308f1024cb7c01e58c4b4f7a7fc7
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-06 16:07:06 -0300

    chore(config.settings): add env vars to STATIC_URL and MEDIA_URL

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,10 +14,6 @@ DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
-# Media
-
-MEDIA_URL = "/killay/media/"
-
 # Caches
 
 CACHES = {

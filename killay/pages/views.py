c3160f114a8042c771da6162c476aa4f82eb2a3b
Sergio Acuña <sergioacunao@protonmail.com>
2024-06-13 16:55:31 -0400

    chore(pages.views): add redirection to link page

--- killay/pages/views.py
+++ killay/pages/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView
+from django.shortcuts import redirect
 
+from killay.pages.lib.constants import PageConstants
 from killay.pages.models import Page
 
 from killay.admin.views.mixins import PublishRequiredMixin
@@ -9,6 +11,12 @@ class PageDetailView(PublishRequiredMixin, DetailView):
     model = Page
     slug_field = "slug"
     slug_url_kwarg = "slug"
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        if self.object.kind == PageConstants.KIND_LINK and self.object.redirect_to:
+            return redirect(self.object.redirect_to)
+        return response
 
     def get_context_data(self, *args, **kwargs) -> dict:
         context = super().get_context_data(*args, **kwargs)
